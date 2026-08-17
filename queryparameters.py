7ecc4e17b77638918d392cf2e0487121b668a895
Sanatan Bramhane <bsanatan51@gmail.com>
2026-08-17 21:21:03 +0530

    leaarned about query paramters aand optional parameters

--- queryparameters.py
+++ queryparameters.py
@@ -1,1 +1,27 @@
 #query parameters and optional parameters
+
+#query parameters are the extra data in the end of the url
+#/users?name=mohit
+#/products?price=1000
+#the query after ? is known as query parameter
+
+#the example of query parameter is amazon filtering the filtering is done on the basis of query parameters
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/users")
+def get_users(name:str = None):
+    return{"Name":name}
+
+@app.get("/products")
+def get_products(limit:int = None):
+    return{"limit":limit}
+
+@app.get("/items")
+def get_products(name:str = None, price:int=0):
+    return{
+        "Name":name,
+        "Price":price
+        }
